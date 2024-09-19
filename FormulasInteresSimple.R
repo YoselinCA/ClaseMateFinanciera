a91af8f59375910e7b4da9f5286d20a234ca5f63
YoselinCA <2350492h@umich.mx>
2024-09-19 12:40:54 -0600

    Carga Tarea version 1.0
    En esta carga se sube la primera versión de la tarea de interés simple.

---
+++ FormulasInteresSimple.R
@@ -0,0 +1,11 @@
+# Se presentan las funciones de interés simple.
+# Autor: Yoselin Castañeda Ayllón.
+# V 1.0: 19 de septiembre de 2024.
+
+# Valor futuro con interés simple:
+
+valorFinalSimple=function(VA,R,T){
+  xSalida=VA*(1+(r*t))
+  
+  return(xSalida)
+}
